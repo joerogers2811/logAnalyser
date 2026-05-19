c14c770291e12e6bdbc1aa412ae875cb5cb28ba4
joerogers2811 <joerogers2811@gmail.com>
2026-05-19 21:31:17 +0100

    initial bootstrap

---
+++ src/test/java/com/sre/triage/loganalyser/LoganalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sre.triage.loganalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoganalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
